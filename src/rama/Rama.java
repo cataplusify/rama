38391a8eb60b8c52e185684d506fb5e00ef79f3a
203pc19 <203pc19@203pc15-PC>
2016-01-26 11:59:25 +0100

    1version

---
+++ src/rama/Rama.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rama;
+
+/**
+ *
+ * @author 203pc19
+ */
+public class Rama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public void rama1() {
+        System.out.println("PRIMERA RAMA");
+    }
+    
+    public void rama2() {
+        System.out.println("SEGUNDA RAMA");
+    }
+    
+}
